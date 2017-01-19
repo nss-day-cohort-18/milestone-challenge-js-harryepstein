bcf6c2bba4b2d8c882a5de8b58838e106a3e820d
Harry Epstein <harryeps@gmail.com>
2017-01-19 10:50:38 -0600

    challenge completed

--- main.js
+++ main.js
@@ -2,17 +2,33 @@ console.log("this is a test")
 //DECLARE SOME GLOBALS FOR LATER PROCUREMENT FROM THE DOM
 let character = "*";
 let space = " ";
-let height = 7;
+let height = 0;
 let tree = "";
+
+let button = document.getElementById('button');
+
+button.addEventListener('click', function(){
+  let characterInput = document.getElementById("character");
+  let heightInput = document.getElementById("height");
+  console.log('characterInput', characterInput, 'heightInput', heightInput)
+
+  height = heightInput.value;
+  character = characterInput.value;
+  console.log('height', height, 'character', character);
+  createTree(height);
+})
 
 function printSpaces (height){
   for (let spacesLine=0; spacesLine < height; spacesLine++)
     tree += space;
     console.log("inside printSpaces for loop");
+
 }
 
-function printAsterisks (height, line){
+function printAsterisks (height){
+
   for (let asteriskLine=0; asteriskLine < height; asteriskLine++)
+
     tree += character;
     console.log("inside the printAstersk for loop");
 }
@@ -28,14 +44,15 @@ function createTree  (height){
   printSpaces (height-line)
   printAsterisks(line)
   printAsterisks(line-1)
-
+  if (line = height) {
+  baseLength = (printAsterisks(line).length + printAsterisks(line-1).length)
+}
   tree += "\n";
   console.log("inside the createTree function")
 }
 buildAtrunk();
-console.log("here follows the tree:", tree)
+console.log(tree);
 }
 
-createTree (height);
 // Invoke the functio
 console.log("this is the end of the file");
